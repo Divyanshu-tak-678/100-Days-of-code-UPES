1afeef8d2993229f1b4b9adfcdeb2d092c7fb9d8
Divyanshu-tak-678 <Divyanshu.32721@stu.upes.ac.in>
2026-08-11 19:15:00 +0530

    Day 02: User Inputs, Operations & Output (Q3, Q4)

---
+++ Question_3_Day 2.c
@@ -0,0 +1,20 @@
+/*
+ * UPES 100 Days of Code Challenge
+ * Day 02 | Date: 11-August-2026
+ * Section: User Inputs, Operations & Output
+ * 
+ * Question 3:
+ * Write a program to calculate the area and perimeter of a rectangle given its length and breadth.
+ */
+
+#include <stdio.h>
+int main(){
+
+float r;
+
+ printf("what is the radius of your circle?");
+ scanf ("%f", &r ); 
+ printf("The area of the circle is %f and the circumfrence is %f", 3.14*r*r, 2*3.14*r);
+	
+	return 0;
+}
---
+++ Question_4_Day 2.c
@@ -0,0 +1,21 @@
+/*
+ * UPES 100 Days of Code Challenge
+ * Day 02 | Date: 11-August-2026
+ * Section: User Inputs, Operations & Output
+ * 
+ * Question 4:
+ * Write a program to calculate the area and circumference of a circle given its radius.
+ */
+
+#include <stdio.h>
+int main(){
+
+int b, l;
+ printf("what is the lenght and breadth of your rectangle?");
+ scanf ("%d %d", &l, &b );
+
+ printf("The area of the rectangle is %d and the parameter is %d", l*b, 2*(l+b));
+
+	
+	return 0;
+}
